3a4b7d43effb86faa532cae6ba11029a6bf4a410
Bharat Agarwal <43988371+bharatagsrwal@users.noreply.github.com>
2019-03-15 00:41:53 +0530

    Changed UI of details page

--- lib/userDetails.dart
+++ lib/userDetails.dart
@@ -15,54 +15,76 @@ class Details extends StatelessWidget {
       ),
       body: new ListView(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: new Stack(
-              children: <Widget>[
-              new Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new Column(
-                  children: <Widget>[
-                    new Hero(
-                      tag: "image$index",
-                      child: CircleAvatar(
-                        radius: 100.0,
-                        backgroundImage: new NetworkImage(user.pictureLarge),
+          new Stack(
+            children: <Widget>[
+              new Container(
+                padding: EdgeInsets.all(0.0),
+                height: MediaQuery.of(context).size.height * 0.16,
+                decoration: new BoxDecoration(color: Colors.blue),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0,right: 8.0),
+                child: Container(
+                  padding: new EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.15),
+                  child: new Card(
+                    child: Container(
+                      padding: new EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.15,
+                          bottom: MediaQuery.of(context).size.height * 0.05),
+                      child: new Column(
+                        children: <Widget>[
+                          new Row(
+                            children: <Widget>[
+                              new Expanded(
+                                child: SizedBox(
+                                  height: 10.0,
+                                  width: 500.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          new Text(
+                            user.fullName(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 40.0),
+                          ),
+                          new Row(
+                            children: <Widget>[
+                              new Expanded(
+                                child: new SizedBox(
+                                  height: 10.0,
+                                  width: 500.0,
+                                ),
+                              )
+                            ],
+                          ),
+                          new Text(
+                            user.email,
+                            style: new TextStyle(color: Colors.grey),
+                          ),
+                        ],
                       ),
                     ),
-                    new Row(
-                      children: <Widget>[
-                        new Expanded(
-                          child: SizedBox(
-                            height: 10.0,
-                            width: 500.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                    new Text(
-                      user.fullName(),
-                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40.0),
-                    ),
-                    new Row(
-                      children: <Widget>[
-                        new Expanded(
-                          child: new SizedBox(
-                            height: 10.0,
-                            width: 500.0,
-                          ),
-                        )
-                      ],
-                    ),
-                    new Text(user.email,style: new TextStyle(color: Colors.grey),),
-                  ],
+                    elevation: 10.0,
+                  ),
                 ),
               ),
-              elevation: 10.0,
-            ),
-              ],
-            )
+              Container(
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.02),
+                child: new Center(
+                  child: new Hero(
+                    tag: "image$index",
+                    child: CircleAvatar(
+                      radius: 100.0,
+                      backgroundImage: new NetworkImage(user.pictureLarge),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
